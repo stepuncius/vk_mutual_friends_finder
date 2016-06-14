0cc24feff4fec2ab9d3564027d8934d2da0dbcc6
Stepan Kholodov <holodovsya@gmail.com>
2016-06-14 22:33:23 +0300

    get_names_of_users method added

---
+++ tests/test_get_names_of_users.py
@@ -0,0 +1,15 @@
+import collections
+import pytest
+import vk_mutual_friends_finder.get_names_of_users as gnfu
+
+
+def test_get_names_of_friends():
+    with pytest.raises(TypeError):
+        gnfu.get_names_of_users()
+    with pytest.raises(AssertionError):
+        gnfu.get_names_of_users([1, 2, 3])
+    durov = gnfu.get_names_of_users(set((1, )))[0]
+    assert isinstance(durov, tuple),\
+        "Function must return namedtuple.".format(type_of_result)
+    assert durov.last_name == "Дуров" and durov.first_name == "Павел",\
+        "Wrong result returned or Pavel Durov changed his name :)"
---
+++ vk_mutual_friends_finder/get_names_of_users.py
@@ -0,0 +1,28 @@
+import collections
+import pyvkontakte
+
+
+def get_names_of_users(set_of_users):
+    """Gets set of user's ids and returns namedtuple
+        with their names, last names and link on their pages.
+
+        Caution: It can't work with more than 1000 people,
+        it's vkapi's feauture.
+    """
+    assert type(set_of_users) == set, "Not set given"
+    if (len(set_of_users) > 1000):
+        print("only first thousand of users will be shown.")
+    api = pyvkontakte.VkontakteApi()
+    string_of_ids = ",".join(map(str, set_of_users))
+    response = api.call("users.get", user_ids=string_of_ids)
+    user = collections.namedtuple(
+        'user', ['adress', 'first_name', 'last_name'])
+    result = [user(
+        adress=usr['id'],
+        first_name=usr['first_name'],
+        last_name=usr['last_name']
+        )
+        for usr in response]
+    return result
+if __name__ == "__main__":
+    print(get_names_of_users(set((1, 3, 6))))
