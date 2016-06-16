9fdb0bf05422e5e77b76b91bc44442765953e7e8
Stepan Kholodov <holodovsya@gmail.com>
2016-06-16 19:02:29 +0300

    now __main__.py takes links on vk as attributes

--- vk_mutual_friends_finder/__main__.py
+++ vk_mutual_friends_finder/__main__.py
@@ -1,11 +1,13 @@
 import sys
-
+import get_id_by_link as gidb
 import get_names_of_users
 import intersect_friends
 
 
 if __name__ == "__main__":
-    ids = intersect_friends.intersect_friends(sys.argv[1:])
+    ids = intersect_friends.intersect_friends(list(
+        map(lambda x:
+            gidb.get_id_by_link(x) if "vk.com" in x else x, sys.argv[1:])))
     result = get_names_of_users.get_names_of_users(ids)
     for user in result:
         print(user.first_name, user.last_name, user.adress)
