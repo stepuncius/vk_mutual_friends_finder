928ce6f99b569d073ce88d9ca551758c49e9105e
Stepan Kholodov <holodovsya@gmail.com>
2016-06-16 22:03:54 +0300

    bug fixed: FriendsGetter.get_friends() now correctly works with aliases

--- vk_mutual_friends_finder/get_names_of_users.py
+++ vk_mutual_friends_finder/get_names_of_users.py
@@ -15,11 +15,12 @@ def get_names_of_users(set_of_users):
         print("only first thousand of users will be shown.")
     api = pyvkontakte.VkontakteApi()
     string_of_ids = ",".join(map(str, set_of_users))
-    response = api.call("users.get", user_ids=string_of_ids)
+    response = api.call("users.get", user_ids=string_of_ids, v='5.8')
     user = collections.namedtuple(
-        'user', ['adress', 'first_name', 'last_name'])
+        'user', ['adress', 'first_name', 'last_name', 'id'])
     result = [user(
         adress=VK_ADRESS + str(usr['id']),
+        id=usr['id'],
         first_name=usr['first_name'],
         last_name=usr['last_name']
         )
