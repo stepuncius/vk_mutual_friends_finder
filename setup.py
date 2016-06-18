96a308c2b5b0169bd1878aa8a7d99cef0657536e
Stepan Kholodov <holodovsya@gmail.com>
2016-06-18 18:51:06 +0300

    setup.py added

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 setup(
-    name = "vk_mutual_friends_finder",
-    packages = ["vk_mutual_friends_finder"],
-    version = "1.0",
-    description = "Finds mutual friends in social network 'vkontakte' (https://vk.com)",
-    author = "Stepan Kholodov",
-    url = "https://github.com/stepuncius/vk_mutual_friends_finder",
-    classifiers = [
+    name="vk_mutual_friends_finder",
+    packages=["vk_mutual_friends_finder"],
+    version="1.0",
+    description="""Finds mutual friends in social network
+        'vkontakte' (https://vk.com)""",
+    author="Stepan Kholodov",
+    url="https://github.com/stepuncius/vk_mutual_friends_finder",
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Intended Audience :: Developers and linux power users",
